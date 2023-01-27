063c12cf6cfaf69693d97dabaffb7be9e03d30f6
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-27 19:28:32 +0700

    Entity getter and setter

--- src/Entity/Todo.php
+++ src/Entity/Todo.php
@@ -1,3 +1,26 @@
-<?php 
+<?php
 
 namespace App;
+
+class Todo
+{
+    public function __construct(private string $todo, private int $id)
+    {
+    }
+    public function getTodo(): string
+    {
+        return $this->todo;
+    }
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    public function setTodo(string $todo): void
+    {
+        $this->todo = $todo;
+    }
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+}
