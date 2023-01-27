77720ce02fceaf00ce0413ae91db000ea050c4a2
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-28 00:58:57 +0700

    Todo View

--- src/View/TodolistView.php
+++ src/View/TodolistView.php
@@ -3,6 +3,7 @@
 namespace App\View;
 
 use App\Service\TodolistService;
+use App\Helper\Input;
 
 class TodolistView
 {
@@ -18,6 +19,31 @@ class TodolistView
             echo "1. Tambah Todolist" . PHP_EOL;
             echo "2. Hapus Todolist" . PHP_EOL;
             echo "x. Keluar" . PHP_EOL;
+            $input = Input::get("Masukan Nomor Pilihan : ");
+            if ($input == "1") {
+                $this->addTodolist();
+            } else if ($input == "2") {
+                $this->removeTodolist();
+            } else if ($input = "x") {
+                break;
+            } else {
+                echo "Pilihanmu salah!" . PHP_EOL;
+            }
         }
+        echo "Terima Kasih, sampai jumpa !" . PHP_EOL;
+    }
+    public function addTodolist(): void
+    {
+        echo "Tambah Todolist" . PHP_EOL;
+        echo "===========" . PHP_EOL;
+        $input = Input::get("Masukan todolist kamu : ");
+        $this->todolistService->showAdd($input);
+    }
+    public function removeTodolist(): void
+    {
+        echo "Hapus Todolist" . PHP_EOL;
+        echo "===========" . PHP_EOL;
+        $input = Input::get("Masukan nomor todolist yang dihapus : ");
+        $this->todolistService->showRemove($input);
     }
 }
